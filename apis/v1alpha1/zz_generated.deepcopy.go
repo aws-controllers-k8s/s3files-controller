sapphirew/ack-s3files-controller

feat(s3files): Add FileSystem resource to s3files-controller
Configure FileSystem in generator.yaml with immutable fields (Bucket,
KMSKeyID, Prefix, RoleARN, AcceptBucketWarning), read-only status
fields (FileSystemID, FileSystemARN, LifeCycleState, etc.), custom
fields for Policy (IAM) and SynchronizationConfiguration sub-resources,
tagging support, error handling, and synced.when lifecycle condition.

Add custom hook templates for sub-resource management:
- sdk_read_one_post_set_output: fetches Policy and SyncConfig,
  sets terminal condition on error lifecycle state
- sdk_create_post_set_output: no-op, sub-resources applied after
  FileSystem reaches available state
- hooks.go with customUpdateFileSystem: handles Policy and
  SynchronizationConfiguration updates via sub-resource APIs

Run code generation to produce CRDs, API types, controller logic,
RBAC, and Helm chart artifacts.

apis/v1alpha1/zz_generated.deepcopy.go
@@ -1,0 +1,436 @@
+//go:build !ignore_autogenerated
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Code generated by ack-generate. DO NOT EDIT.
+
+// Code generated by controller-gen. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	corev1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExpirationDataRule) DeepCopyInto(out *ExpirationDataRule) {
+	*out = *in
+	if in.DaysAfterLastAccess != nil {
+		in, out := &in.DaysAfterLastAccess, &out.DaysAfterLastAccess
+		*out = new(int64)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExpirationDataRule.
+func (in *ExpirationDataRule) DeepCopy() *ExpirationDataRule {
+	if in == nil {
+		return nil
+	}
+	out := new(ExpirationDataRule)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystem) DeepCopyInto(out *FileSystem) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystem.
+func (in *FileSystem) DeepCopy() *FileSystem {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystem)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *FileSystem) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystemList) DeepCopyInto(out *FileSystemList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]FileSystem, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystemList.
+func (in *FileSystemList) DeepCopy() *FileSystemList {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystemList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *FileSystemList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystemSpec) DeepCopyInto(out *FileSystemSpec) {
+	*out = *in
+	if in.AcceptBucketWarning != nil {
+		in, out := &in.AcceptBucketWarning, &out.AcceptBucketWarning
+		*out = new(bool)
+		**out = **in
+	}
+	if in.Bucket != nil {
+		in, out := &in.Bucket, &out.Bucket
+		*out = new(string)
+		**out = **in
+	}
+	if in.ExpirationDataRules != nil {
+		in, out := &in.ExpirationDataRules, &out.ExpirationDataRules
+		*out = make([]*ExpirationDataRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(ExpirationDataRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.ImportDataRules != nil {
+		in, out := &in.ImportDataRules, &out.ImportDataRules
+		*out = make([]*ImportDataRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(ImportDataRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.KMSKeyID != nil {
+		in, out := &in.KMSKeyID, &out.KMSKeyID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Policy != nil {
+		in, out := &in.Policy, &out.Policy
+		*out = new(string)
+		**out = **in
+	}
+	if in.Prefix != nil {
+		in, out := &in.Prefix, &out.Prefix
+		*out = new(string)
+		**out = **in
+	}
+	if in.RoleARN != nil {
+		in, out := &in.RoleARN, &out.RoleARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.Tags != nil {
+		in, out := &in.Tags, &out.Tags
+		*out = make([]*Tag, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(Tag)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystemSpec.
+func (in *FileSystemSpec) DeepCopy() *FileSystemSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystemSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystemStatus) DeepCopyInto(out *FileSystemStatus) {
+	*out = *in
+	if in.ACKResourceMetadata != nil {
+		in, out := &in.ACKResourceMetadata, &out.ACKResourceMetadata
+		*out = new(corev1alpha1.ResourceMetadata)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]*corev1alpha1.Condition, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(corev1alpha1.Condition)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.CreationTime != nil {
+		in, out := &in.CreationTime, &out.CreationTime
+		*out = (*in).DeepCopy()
+	}
+	if in.FileSystemARN != nil {
+		in, out := &in.FileSystemARN, &out.FileSystemARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.LatestVersionNumber != nil {
+		in, out := &in.LatestVersionNumber, &out.LatestVersionNumber
+		*out = new(int64)
+		**out = **in
+	}
+	if in.LifeCycleState != nil {
+		in, out := &in.LifeCycleState, &out.LifeCycleState
+		*out = new(string)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.StatusMessage != nil {
+		in, out := &in.StatusMessage, &out.StatusMessage
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystemStatus.
+func (in *FileSystemStatus) DeepCopy() *FileSystemStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystemStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ImportDataRule) DeepCopyInto(out *ImportDataRule) {
+	*out = *in
+	if in.Prefix != nil {
+		in, out := &in.Prefix, &out.Prefix
+		*out = new(string)
+		**out = **in
+	}
+	if in.SizeLessThan != nil {
+		in, out := &in.SizeLessThan, &out.SizeLessThan
+		*out = new(int64)
+		**out = **in
+	}
+	if in.Trigger != nil {
+		in, out := &in.Trigger, &out.Trigger
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ImportDataRule.
+func (in *ImportDataRule) DeepCopy() *ImportDataRule {
+	if in == nil {
+		return nil
+	}
+	out := new(ImportDataRule)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ListAccessPointsDescription) DeepCopyInto(out *ListAccessPointsDescription) {
+	*out = *in
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ListAccessPointsDescription.
+func (in *ListAccessPointsDescription) DeepCopy() *ListAccessPointsDescription {
+	if in == nil {
+		return nil
+	}
+	out := new(ListAccessPointsDescription)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ListFileSystemsDescription) DeepCopyInto(out *ListFileSystemsDescription) {
+	*out = *in
+	if in.Bucket != nil {
+		in, out := &in.Bucket, &out.Bucket
+		*out = new(string)
+		**out = **in
+	}
+	if in.CreationTime != nil {
+		in, out := &in.CreationTime, &out.CreationTime
+		*out = (*in).DeepCopy()
+	}
+	if in.FileSystemARN != nil {
+		in, out := &in.FileSystemARN, &out.FileSystemARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.RoleARN != nil {
+		in, out := &in.RoleARN, &out.RoleARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+	if in.StatusMessage != nil {
+		in, out := &in.StatusMessage, &out.StatusMessage
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ListFileSystemsDescription.
+func (in *ListFileSystemsDescription) DeepCopy() *ListFileSystemsDescription {
+	if in == nil {
+		return nil
+	}
+	out := new(ListFileSystemsDescription)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ListMountTargetsDescription) DeepCopyInto(out *ListMountTargetsDescription) {
+	*out = *in
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+	if in.StatusMessage != nil {
+		in, out := &in.StatusMessage, &out.StatusMessage
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ListMountTargetsDescription.
+func (in *ListMountTargetsDescription) DeepCopy() *ListMountTargetsDescription {
+	if in == nil {
+		return nil
+	}
+	out := new(ListMountTargetsDescription)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Tag) DeepCopyInto(out *Tag) {
+	*out = *in
+	if in.Key != nil {
+		in, out := &in.Key, &out.Key
+		*out = new(string)
+		**out = **in
+	}
+	if in.Value != nil {
+		in, out := &in.Value, &out.Value
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Tag.
+func (in *Tag) DeepCopy() *Tag {
+	if in == nil {
+		return nil
+	}
+	out := new(Tag)
+	in.DeepCopyInto(out)
+	return out
+}

aws-controllers-k8s/s3files-controller

apis/v1alpha1/zz_generated.deepcopy.go
@@ -1,0 +1,446 @@
+//go:build !ignore_autogenerated
+
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+// Code generated by ack-generate. DO NOT EDIT.
+
+// Code generated by controller-gen. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	corev1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ExpirationDataRule) DeepCopyInto(out *ExpirationDataRule) {
+	*out = *in
+	if in.DaysAfterLastAccess != nil {
+		in, out := &in.DaysAfterLastAccess, &out.DaysAfterLastAccess
+		*out = new(int64)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ExpirationDataRule.
+func (in *ExpirationDataRule) DeepCopy() *ExpirationDataRule {
+	if in == nil {
+		return nil
+	}
+	out := new(ExpirationDataRule)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystem) DeepCopyInto(out *FileSystem) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	in.Status.DeepCopyInto(&out.Status)
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystem.
+func (in *FileSystem) DeepCopy() *FileSystem {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystem)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *FileSystem) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystemList) DeepCopyInto(out *FileSystemList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]FileSystem, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystemList.
+func (in *FileSystemList) DeepCopy() *FileSystemList {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystemList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *FileSystemList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystemSpec) DeepCopyInto(out *FileSystemSpec) {
+	*out = *in
+	if in.AcceptBucketWarning != nil {
+		in, out := &in.AcceptBucketWarning, &out.AcceptBucketWarning
+		*out = new(bool)
+		**out = **in
+	}
+	if in.Bucket != nil {
+		in, out := &in.Bucket, &out.Bucket
+		*out = new(string)
+		**out = **in
+	}
+	if in.BucketRef != nil {
+		in, out := &in.BucketRef, &out.BucketRef
+		*out = new(corev1alpha1.AWSResourceReferenceWrapper)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.ExpirationDataRules != nil {
+		in, out := &in.ExpirationDataRules, &out.ExpirationDataRules
+		*out = make([]*ExpirationDataRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(ExpirationDataRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.ImportDataRules != nil {
+		in, out := &in.ImportDataRules, &out.ImportDataRules
+		*out = make([]*ImportDataRule, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(ImportDataRule)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.KMSKeyID != nil {
+		in, out := &in.KMSKeyID, &out.KMSKeyID
+		*out = new(string)
+		**out = **in
+	}
+	if in.KMSKeyRef != nil {
+		in, out := &in.KMSKeyRef, &out.KMSKeyRef
+		*out = new(corev1alpha1.AWSResourceReferenceWrapper)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Policy != nil {
+		in, out := &in.Policy, &out.Policy
+		*out = new(string)
+		**out = **in
+	}
+	if in.Prefix != nil {
+		in, out := &in.Prefix, &out.Prefix
+		*out = new(string)
+		**out = **in
+	}
+	if in.RoleARN != nil {
+		in, out := &in.RoleARN, &out.RoleARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.RoleRef != nil {
+		in, out := &in.RoleRef, &out.RoleRef
+		*out = new(corev1alpha1.AWSResourceReferenceWrapper)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Tags != nil {
+		in, out := &in.Tags, &out.Tags
+		*out = make([]*Tag, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(Tag)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystemSpec.
+func (in *FileSystemSpec) DeepCopy() *FileSystemSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystemSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *FileSystemStatus) DeepCopyInto(out *FileSystemStatus) {
+	*out = *in
+	if in.ACKResourceMetadata != nil {
+		in, out := &in.ACKResourceMetadata, &out.ACKResourceMetadata
+		*out = new(corev1alpha1.ResourceMetadata)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.Conditions != nil {
+		in, out := &in.Conditions, &out.Conditions
+		*out = make([]*corev1alpha1.Condition, len(*in))
+		for i := range *in {
+			if (*in)[i] != nil {
+				in, out := &(*in)[i], &(*out)[i]
+				*out = new(corev1alpha1.Condition)
+				(*in).DeepCopyInto(*out)
+			}
+		}
+	}
+	if in.CreationTime != nil {
+		in, out := &in.CreationTime, &out.CreationTime
+		*out = (*in).DeepCopy()
+	}
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.LatestVersionNumber != nil {
+		in, out := &in.LatestVersionNumber, &out.LatestVersionNumber
+		*out = new(int64)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+	if in.StatusMessage != nil {
+		in, out := &in.StatusMessage, &out.StatusMessage
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new FileSystemStatus.
+func (in *FileSystemStatus) DeepCopy() *FileSystemStatus {
+	if in == nil {
+		return nil
+	}
+	out := new(FileSystemStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ImportDataRule) DeepCopyInto(out *ImportDataRule) {
+	*out = *in
+	if in.Prefix != nil {
+		in, out := &in.Prefix, &out.Prefix
+		*out = new(string)
+		**out = **in
+	}
+	if in.SizeLessThan != nil {
+		in, out := &in.SizeLessThan, &out.SizeLessThan
+		*out = new(int64)
+		**out = **in
+	}
+	if in.Trigger != nil {
+		in, out := &in.Trigger, &out.Trigger
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ImportDataRule.
+func (in *ImportDataRule) DeepCopy() *ImportDataRule {
+	if in == nil {
+		return nil
+	}
+	out := new(ImportDataRule)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ListAccessPointsDescription) DeepCopyInto(out *ListAccessPointsDescription) {
+	*out = *in
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ListAccessPointsDescription.
+func (in *ListAccessPointsDescription) DeepCopy() *ListAccessPointsDescription {
+	if in == nil {
+		return nil
+	}
+	out := new(ListAccessPointsDescription)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ListFileSystemsDescription) DeepCopyInto(out *ListFileSystemsDescription) {
+	*out = *in
+	if in.Bucket != nil {
+		in, out := &in.Bucket, &out.Bucket
+		*out = new(string)
+		**out = **in
+	}
+	if in.CreationTime != nil {
+		in, out := &in.CreationTime, &out.CreationTime
+		*out = (*in).DeepCopy()
+	}
+	if in.FileSystemARN != nil {
+		in, out := &in.FileSystemARN, &out.FileSystemARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Name != nil {
+		in, out := &in.Name, &out.Name
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.RoleARN != nil {
+		in, out := &in.RoleARN, &out.RoleARN
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+	if in.StatusMessage != nil {
+		in, out := &in.StatusMessage, &out.StatusMessage
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ListFileSystemsDescription.
+func (in *ListFileSystemsDescription) DeepCopy() *ListFileSystemsDescription {
+	if in == nil {
+		return nil
+	}
+	out := new(ListFileSystemsDescription)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *ListMountTargetsDescription) DeepCopyInto(out *ListMountTargetsDescription) {
+	*out = *in
+	if in.FileSystemID != nil {
+		in, out := &in.FileSystemID, &out.FileSystemID
+		*out = new(string)
+		**out = **in
+	}
+	if in.OwnerID != nil {
+		in, out := &in.OwnerID, &out.OwnerID
+		*out = new(string)
+		**out = **in
+	}
+	if in.Status != nil {
+		in, out := &in.Status, &out.Status
+		*out = new(string)
+		**out = **in
+	}
+	if in.StatusMessage != nil {
+		in, out := &in.StatusMessage, &out.StatusMessage
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new ListMountTargetsDescription.
+func (in *ListMountTargetsDescription) DeepCopy() *ListMountTargetsDescription {
+	if in == nil {
+		return nil
+	}
+	out := new(ListMountTargetsDescription)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *Tag) DeepCopyInto(out *Tag) {
+	*out = *in
+	if in.Key != nil {
+		in, out := &in.Key, &out.Key
+		*out = new(string)
+		**out = **in
+	}
+	if in.Value != nil {
+		in, out := &in.Value, &out.Value
+		*out = new(string)
+		**out = **in
+	}
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new Tag.
+func (in *Tag) DeepCopy() *Tag {
+	if in == nil {
+		return nil
+	}
+	out := new(Tag)
+	in.DeepCopyInto(out)
+	return out
+}